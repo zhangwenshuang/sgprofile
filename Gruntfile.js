5cacd8a8d22e291e3262be4bb02ac5e725857619
zhangwenshuang <zhangwenshuang@edaijia-inc.cn>
2015-10-20 13:14:59 +0800

    配置sublimegrunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,5 +63,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-watch");
 	grunt.loadNpmTasks("grunt-contrib-jade");
 
-	grunt.registerTask("default", ["watch"]);//"uglify", "htmlmin", "concat", 
+	grunt.registerTask("default", ["watch"]);//"uglify", "htmlmin", "concat",
 };
